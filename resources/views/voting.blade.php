23bd9b9b3351f7cd733e5590d77c18ce6ea9a50e
jef <jefc_uk@hotmail.com>
2020-05-03 08:18:09 +0100

    Capture list changes

--- resources/views/voting.blade.php
+++ resources/views/voting.blade.php
@@ -8,7 +8,8 @@
   <span>
       <h1>Welcome, {{ $votingCountry->voter_name }}, representative of {{ $votingCountry->name }}
         <img class="float-right" src="{{ $votingCountry->getFlagUrl() }}">
-      </h1>      
+
+      </h1>
     </span>
 
 
@@ -18,28 +19,32 @@
       @foreach($scores as $score)
       <li class="">
         {{ $score }}
-        
+
       </li>
-      @endforeach  
+      @endforeach
     </ul>
       </div>
       <div class="col-sm">
       <ul class="list-unstyled countries float-left">
       @foreach($countries->sortBy('name') as $country)
-      <li class="">
+      <li id="country-{{ $country->id }}" data-id="{{ $country->id }}" class="">
         {{ $country->name }}
         <img class="float-right" src="{{ $country->getFlagUrl() }}">
       </li>
-      @endforeach  
+      @endforeach
     </ul>
       </div>
       <div class="col-sm">
-        
+
       </div>
     </div>
 
-
-
+    <button type="submit" class="btn btn-primary">Submit scores</button>
+    <img class="float-right spinner" src="assets/icons/network_internet_pcs_installer-2.png">
+    <span id="data-saving" class="float-right">Saving...</span>
+    <span id="data-saved" class="float-right">All data saved</span>
+    <span id="data-error" class="float-right">Error saving data - please contact Paul</span>
+    
 
   </div>
   <footer class="taskbar">
@@ -51,13 +56,37 @@
               <a href="#" class="btn start-button"><img src="assets/icons/usb-1.png" class="icon-16"></a>
           </div>
       </div>
-    
+
   </footer>
 </body>
 @if(! $votingCountry->voting_complete)
 <script>
 
-$("ul").sortable();
+$("ul").sortable({
+  stop: function(e, ui) {
+      const positionData = $('ul.countries li').toArray().map((item) => $(item).data("id"));
+
+      $(".spinner").addClass("spin");
+
+      $("span#data-saved").hide();
+
+      $("span#data-saving").show();
+
+      $.post("/save-data", {
+        positionData
+      }, function(rdata) {
+        if (error) {
+
+        }
+        
+        $(".spinner").removeClass("spin");
+
+        $("span#data-saved").show();    
+
+        $("span#data-saving").hide();
+      });
+  }
+});
 
 </script>
 @endif
@@ -65,7 +94,7 @@ $("ul").sortable();
 
 li {
   padding: 0;
-  height: 40px;  
+  height: 40px;
   border: 1px solid black;
   padding: 5px;
   background: rgba(255,255,255,0.7);
@@ -85,6 +114,30 @@ li img {
 
 div.col-sm {
   padding: 0;
+}
+
+span#data-saving, span#data-error {
+  display: none;  
+}
+
+.spin {  
+  animation-name: spin;
+  animation-duration: 1500ms;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear; 
+  /* transform: rotate(3deg); */
+   /* transform: rotate(0.3rad);/ */
+   /* transform: rotate(3grad); */ 
+   /* transform: rotate(.03turn);  */
+}
+
+@keyframes spin {
+    from {
+        transform:rotate(0deg);
+    }
+    to {
+        transform:rotate(360deg);
+    }
 }
 </style>
 
