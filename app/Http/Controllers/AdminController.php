35024f6ebfa3e76bedbcd2b53f5a8549477d52a0
jef <jefc_uk@hotmail.com>
2020-05-06 22:07:40 +0100

    Downloading windows friendly

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Country;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
@@ -66,45 +67,45 @@ class AdminController extends Controller
             $columns[] = "Pts to $i";
         }
 
-        $callback = function () use ($countries, $columns) {
-            function fputcsv_eol($handle, $array, $delimiter = ',', $enclosure = '"', $eol = "\n")
-            {
-                $return = fputcsv($handle, $array, $delimiter, $enclosure);
-                if ($return !== false && "\n" != $eol && 0 === fseek($handle, -1, SEEK_CUR)) {
-                    fwrite($handle, $eol);
-                }
-                return $return;
+        function fputcsv_eol($handle, $array, $delimiter = ',', $enclosure = '"', $eol = "\r\n")
+        {
+            $return = fputcsv($handle, $array, $delimiter, $enclosure);
+            if ($return !== false && "\n" != $eol && 0 === fseek($handle, -1, SEEK_CUR)) {
+                fwrite($handle, $eol);
+            }
+            return $return;
+        }
+
+        $filepath = 'public/eurovision-2020-export-'.time().'.csv';
+
+        $file = fopen(storage_path('app/'.$filepath), 'w');
+
+        fputcsv_eol($file, $columns);
+
+        $i = 1;
+
+        foreach ($countries as $country) {
+            $row = [
+                $i,
+                $country->name,
+                $country->flag,
+                $country->voter_name,
+                $country->song_seq,
+                '',
+                $country->voting_complete
+            ];
+
+            foreach ($country->pointsToCountries($countries->where('votable', '=', 1)) as $pointsToCountry) {
+                $row[] = $pointsToCountry;
             }
 
-            $file = fopen('php://output', 'w');
+            fputcsv_eol($file, $row);
 
-            fputcsv($file, $columns);
+            $i++;
+        }
 
-            $i = 1;
+        fclose($file);
 
-            foreach ($countries as $country) {
-                $row = [
-                    $i,
-                    $country->name,
-                    $country->flag,
-                    $country->voter_name,
-                    $country->song_seq,
-                    '',
-                    $country->voting_complete
-                ];
-
-                foreach ($country->pointsToCountries($countries->where('votable', '=', 1)) as $pointsToCountry) {
-                    $row[] = $pointsToCountry;
-                }
-
-                fputcsv_eol($file, $row);
-
-                $i++;
-            }
-
-            fclose($file);
-        };
-
-        return response()->stream($callback, 200, $headers);
+        return Storage::download($filepath, 'dDDDDddd');
     }
 }
